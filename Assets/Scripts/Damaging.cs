508f2ee71dcde96bd50e46817981d082fbec87b7
David Mendes <david.f.mendes@proton.me>
2024-02-06 21:30:45 +0000

    Updated Damaging.cs

--- Assets/Scripts/Damaging.cs
+++ Assets/Scripts/Damaging.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Damaging : MonoBehaviour
 {
+    [SerializeField] private int damagePerHit = 1;
+    
+    public int DamagePerHit { get; private set; }
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        DamagePerHit = damagePerHit;
     }
 
     // Update is called once per frame
