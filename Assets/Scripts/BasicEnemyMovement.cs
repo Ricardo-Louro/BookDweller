07407673d2e1dfc345c237bd6a6f5d50b6349cee
David Mendes <david.f.mendes@proton.me>
2024-02-06 20:32:56 +0000

    Added BasicEnemyScript

---
+++ Assets/Scripts/BasicEnemyMovement.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1;
+    [SerializeField] private Transform playerTransform;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _moveDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _moveDirection = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdatePosition()
+    {
+        if (playerTransform is null) _rigidbody.velocity = Vector2.zero;
+        else
+        {
+            _moveDirection = playerTransform.position - transform.position;
+            _moveDirection = _moveDirection.normalized;
+            _rigidbody.velocity = _moveDirection * moveSpeed/ 100 / Time.deltaTime;
+        }
+    }
+}
