26643044fa732476c9f872aa368673c93e52028a
David Mendes <david.f.mendes@proton.me>
2024-02-07 12:29:14 +0000

    Added Stat SubClasses for player and enemies

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class EnemyStats : Stats
+    {
+        [SerializeField] private int spawnValue;
+        [SerializeField] private int scoreValue;
+
+        public int SpawnValue
+        {
+            get => spawnValue;
+            set => spawnValue = value;
+        }
+
+        public int ScoreValue
+        {
+            get => scoreValue;
+            set => scoreValue = value;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerStats : Stats
+    {
+        [SerializeField] private int experiencePoints;
+        
+        public int XP
+        {
+            get => experiencePoints;
+            set => experiencePoints = value;
+        }
+        
+    }
+    
+}
