b2ce6074aee030307f0a74e6f0f8b981f7adf037
David Mendes <david.f.mendes@proton.me>
2024-02-08 01:40:12 +0000

    Added a font, a script, a scene and an image

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    [SerializeField] private Canvas MainMenuCanvas;
+    [SerializeField] private Canvas SettingsCanvas;
+
+    public void ExitButton()
+    {
+        
+    }
+
+    public void SettingsButtons()
+    {
+        
+    }
+
+    public void StartGameButton()
+    {
+        
+    }
+}
